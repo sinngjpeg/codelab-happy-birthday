e9021201aeca1b3b5ecc10f3da42d39c1ba4f592
ingrid silva <ingridsilvatec@outlook.com>
2023-05-04 01:07:35 -0300

    add colum to text messages

--- HappyBirthDay/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthDay/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthDayTheme
+import androidx.compose.foundation.layout.Row
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    BirthdayGreetingWithText( message = "Happy Birthday Ingrid!", from = "- from Cauê")
                 }
             }
         }
@@ -30,17 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 36.sp
-    )
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column() {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthDayTheme {
-        BirthdayGreetingWithText(message = "Happy Birthday Ingrid!")
+        BirthdayGreetingWithText(
+            message = "Happy Birthday Ingrid!",
+            from = "- from Caue"
+        )
     }
 }
